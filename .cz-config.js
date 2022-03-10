95ee47b887af39f1caa966b1564b586286ff4454
薛之谦 <1716148128@qq.com>
2022-03-10 23:36:52 +0800

    feat: 测试 lint-staged
    使用 commitizen 规范代码提交

--- .cz-config.js
+++ .cz-config.js
@@ -1,45 +1,45 @@
 module.exports = {
   // 可选类型
   types: [{
-    value: 'feat',
-    name: 'feat:     新功能'
-  },
-  {
-    value: 'fix',
-    name: 'fix:      修复'
-  },
-  {
-    value: 'docs',
-    name: 'docs:     文档变更'
-  },
-  {
-    value: 'style',
-    name: 'style:    代码格式(不影响代码运行的变动)'
-  },
-  {
-    value: 'refactor',
-    name: 'refactor: 重构(既不是增加feature，也不是修复bug)'
-  },
-  {
-    value: 'perf',
-    name: 'perf:     性能优化'
-  },
-  {
-    value: 'test',
-    name: 'test:     增加测试'
-  },
-  {
-    value: 'chore',
-    name: 'chore:    构建过程或辅助工具的变动'
-  },
-  {
-    value: 'revert',
-    name: 'revert:   回退'
-  },
-  {
-    value: 'build',
-    name: 'build:    打包'
-  }
+      value: 'feat',
+      name: 'feat:     新功能'
+    },
+    {
+      value: 'fix',
+      name: 'fix:      修复'
+    },
+    {
+      value: 'docs',
+      name: 'docs:     文档变更'
+    },
+    {
+      value: 'style',
+      name: 'style:    代码格式(不影响代码运行的变动)'
+    },
+    {
+      value: 'refactor',
+      name: 'refactor: 重构(既不是增加feature，也不是修复bug)'
+    },
+    {
+      value: 'perf',
+      name: 'perf:     性能优化'
+    },
+    {
+      value: 'test',
+      name: 'test:     增加测试'
+    },
+    {
+      value: 'chore',
+      name: 'chore:    构建过程或辅助工具的变动'
+    },
+    {
+      value: 'revert',
+      name: 'revert:   回退'
+    },
+    {
+      value: 'build',
+      name: 'build:    打包'
+    }
   ],
   // 步骤
   messages: {
@@ -50,6 +50,9 @@ module.exports = {
     footer: '请输入要关闭的issue（可选）',
     confirmConmit: '确认要使用以上信息提交?（y/n）'
   },
+  // 跳过问题
+  // skipQuestions: ['body','footer'],
+
   // 默认长度为72
   subjectLimit: 72
 }
