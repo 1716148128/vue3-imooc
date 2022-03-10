3507b95b9797f82ab23ed71ffecb7a93ccc9667b
薛之谦 <1716148128@qq.com>
2022-03-10 21:24:45 +0800

    feat(git): add commitizen
    使用 commitizen 规范代码提交

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -13,6 +13,9 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  created() {
+    console.log('1')
   }
 }
 </script>
