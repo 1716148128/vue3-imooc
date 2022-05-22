a5053b39166cf6699d3189c776f6acbde019dd9e
薛之谦 <1716148128@qq.com>
2022-05-23 00:41:07 +0800

    feat: 完成了角色列表和权限列表的页面展示
    使用 commitizen 规范代码提交

--- src/views/permission-list/index.vue
+++ src/views/permission-list/index.vue
@@ -1,9 +1,46 @@
 <template>
-  <div class="">权限列表</div>
+  <div class="">
+    <el-card>
+      <el-table
+        :data="allPermission"
+        style="width: 100%"
+        border
+        row-key="id"
+        default-expand-all
+        :tree-props="{ children: 'children', hanChildren: 'hasChildren' }"
+      >
+        <el-table-column
+          prop="permissionName"
+          :label="$t('msg.permission.name')"
+          width="180"
+        ></el-table-column>
+        <el-table-column
+          prop="permissionMark"
+          :label="$t('msg.permission.mark')"
+          width="180"
+        ></el-table-column>
+        <el-table-column
+          prop="permissionDesc"
+          :label="$t('msg.permission.desc')"
+          width="180"
+        ></el-table-column>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 
 <script setup>
-import {} from 'vue'
+import { ref } from 'vue'
+import { permissionList } from '@/api/permission'
+import { watchSwitchLang } from '@/utils/i18n'
+
+const allPermission = ref([])
+const getPermissionList = async () => {
+  allPermission.value = await permissionList()
+  console.log(allPermission.value)
+}
+getPermissionList()
+watchSwitchLang(getPermissionList)
 </script>
 
 <style lang="scss" scoped></style>
--- src/views/role-list/index.vue
+++ src/views/role-list/index.vue
@@ -1,9 +1,41 @@
 <template>
-  <div class="">角色列表</div>
+  <div class="">
+    <el-card>
+      <el-table :data="allRoles" border style="width: 100%">
+        <el-table-column
+          type="index"
+          :label="$t('msg.role.index')"
+          width="120"
+        ></el-table-column>
+        <el-table-column
+          :label="$t('msg.role.name')"
+          prop="title"
+        ></el-table-column>
+        <el-table-column
+          :label="$t('msg.role.desc')"
+          prop="describe"
+        ></el-table-column>
+        <el-table-column :label="$t('msg.role.action')" width="200">
+          <el-button type="primary" size="mini">
+            {{ $t('msg.role.assignPermissions') }}
+          </el-button>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 
 <script setup>
-import {} from 'vue'
+import { roleList } from '@/api/role'
+import { watchSwitchLang } from '@/utils/i18n'
+import { ref } from 'vue'
+
+const allRoles = ref([])
+const getRoleList = async () => {
+  allRoles.value = await roleList()
+}
+getRoleList()
+watchSwitchLang(getRoleList)
 </script>
 
 <style lang="scss" scoped></style>
